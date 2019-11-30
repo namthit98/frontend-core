afd83a8577d822d4dec14c54c75591dcfb357977
Namthit98 <namthit98@gmail.com>
2019-11-30 12:51:42 +0700

    tich hop redux logger, redux devtool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,24 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-
-import monitorReducersEnhancer from './enhancers/monitorReducers'
-import loggerMiddleware from './middleware/logger'
+import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './index'
 
+const logger = createLogger({
+  duration: true,
+})
+
 export default function configureStore(preloadedState) {
-  const middlewares = [loggerMiddleware]
+  const middlewares = []
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(logger)
+  }
   const middlewareEnhancer = applyMiddleware(...middlewares)
 
-  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
-  const composedEnhancers = compose(...enhancers)
+  const enhancers = [middlewareEnhancer]
+  const composedEnhancers =
+    process.env.NODE_ENV !== 'production'
+      ? composeWithDevTools(...enhancers)
+      : compose(...enhancers)
 
   const store = createStore(rootReducer, preloadedState, composedEnhancers)
 
