61c654fcf298690b8a3940e755693dfccce70c2c
Namthit98 <namthit98@gmail.com>
2019-11-30 13:08:25 +0700

    tich hop immer

--- src/store/reducers/todo.reducer.js
+++ src/store/reducers/todo.reducer.js
@@ -1,50 +1,30 @@
+import produce from 'immer'
+import { ADD_TODO } from './../actions/actionTypes'
+
 const initialState = {
   todos: [],
-  visibilityFilter: "ALL"
+  visibilityFilter: 'ALL',
 }
 
-function todoReducer(todoState = initialState, action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return addTodo(todoState, action)
-    case 'TOGGLE_STATUS_TODO':
-      return toggleStatus(todoState, action)
-    case 'SHOW_VISIBLE':
-      return showVisible(todoState, action)
-    default:
-      return todoState
-  }
-}
+const todoReducer = (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        draft.todos.push({
+          id: action.payload.id,
+          text: action.payload.text,
+          completed: false,
+        })
+        break
 
-function updateObject(oldObject, newValues) {
-  // Encapsulate the idea of passing a new object as the first parameter
-  // to Object.assign to ensure we correctly copy data instead of mutating
-  return Object.assign({}, oldObject, newValues)
-}
+      case 'TOGGLE_STATUS_TODO':
+        const idx = draft.todos.findIndex(todo => todo.id === action.payload)
+        draft.todos[idx].completed = !draft.todos[idx].completed
+        break
 
-function addTodo(state, action) {
-  const newTodos = state.todos.concat({
-    id: action.payload.id,
-    text: action.payload.text,
-    completed: false,
+      case 'SHOW_VISIBLE':
+        draft.visibilityFilter = action.payload
+    }
   })
-
-  return updateObject(state, { todos: newTodos })
-}
-
-function toggleStatus(state, action) {
-  const idx = state.todos.findIndex(todo => todo.id === action.payload)
-  const cloneTodos = [...state.todos]
-  cloneTodos[idx] = {
-    ...cloneTodos[idx],
-    completed: !cloneTodos[idx].completed
-  }
-
-  return updateObject(state, { todos: cloneTodos })
-}
-
-function showVisible(state, action) {
-  return updateObject(state, { visibilityFilter: action.payload })
-}
 
 export default todoReducer
